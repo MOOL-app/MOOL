3ee1d400a518c4403709af6c11b7b49db00535d3
Leeyoujin00 <cymn6032@gmail.com>
2023-09-21 14:40:23 +0900

    Create spingboot settings

---
+++ src/main/java/com/mool/app/MoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.mool.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoolAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mool/app/MoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mool.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
